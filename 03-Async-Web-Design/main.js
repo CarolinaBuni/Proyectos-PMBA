834e9f5eb544ba743dff4d21e4a5cb7f56852008
Carolina Buni <carolinabuni@gmail.com>
2024-01-11 12:43:41 +0100

    Añado footer

--- 03-Async-Web-Design/main.js
+++ 03-Async-Web-Design/main.js
@@ -1,24 +1,52 @@
 
 import { searchWord } from './src/components/Input/Input';
+import { footer } from './src/pages/Footer/Footer';
 import { divApp, header } from './src/pages/Header/Header';
 import './style.css';
 
+// Función para mostrar imágenes random al cargarse la página
 export const showGallery = async () => {
     const section = document.querySelector('.gallery');
     try {
-        const response = await fetch("https://api.unsplash.com/photos/random?client_id=Apv4rswnoZ0Lp7xGFFJbnS520XpHA1J1dCSILs53y64&count=40");
+        const response = await fetch("https://api.unsplash.com/photos/random?client_id=Apv4rswnoZ0Lp7xGFFJbnS520XpHA1J1dCSILs53y64&count=30");
         const res = await response.json();
+        const imageElements = [];
         for (const c of res) {
+            const imageDiv = document.createElement('div')
             const img = document.createElement("img");
 
             img.classList.add('photo')
             img.src = c.urls.small;
-            section.appendChild(img)
+            imageDiv.classList.add('imgContainer')
+            imageDiv.appendChild(img)
+            section.appendChild(imageDiv)
+            imageElements.push(img); // Agregar las imágenes al array
         }
+         // Función para inicializar Masonry después de que todas las imágenes estén cargadas
+         function initializeMasonry() {
+            const masonry = new Masonry('.gallery', {
+                itemSelector: '.imgContainer',
+                columnWidth: '.imgContainer',
+                gutter: 15
+            });
+        }
+        // Contar las imágenes que están cargadas
+        let imagesLoaded = 0;
+        imageElements.forEach(img => {
+            img.addEventListener('load', () => {
+                imagesLoaded++;
+                if (imagesLoaded === imageElements.length) {
+                    initializeMasonry(); // Inicializar Masonry cuando todas las imágenes estén cargadas
+                }
+            });
+        });
     } catch (error) {
         console.log(error);
     }
 };
+
+document.body.appendChild(footer)
+
 
 
 showGallery()
--- 03-Async-Web-Design/src/components/Input/Input.js
+++ 03-Async-Web-Design/src/components/Input/Input.js
@@ -1,28 +1,105 @@
 import { showGallery } from "../../../main";
 import './Input.css'
 
-export const searchWord = () => {
+// export const searchWord = () => {
+//     const inputHTML = document.querySelector('#buscar');
+//     const section = document.querySelector('.gallery')
+//     let masonry;
+
+//     inputHTML.addEventListener('keyup', async (e) => {
+//         try {
+//             const word = e.target.value;
+//                 if(word === ''){
+//                     showGallery();
+//                 }
+
+//             const newImages = await fetch (`https://api.unsplash.com/search/photos?query=${word}&client_id=Apv4rswnoZ0Lp7xGFFJbnS520XpHA1J1dCSILs53y64&per_page=20`);
+//             const element = await newImages.json();
+
+//             section.innerHTML = '';
+
+//             element.results.forEach(c => {
+//                 const img = document.createElement('img');
+//                 const imgContainer = document.createElement('div')
+
+//                 img.classList.add('photo');
+//                 imgContainer.classList.add('imgContainer')
+
+
+//                 img.src = c.urls.small;
+//                 imgContainer.appendChild(img)
+//                 section.appendChild(imgContainer);
+                
+//             });
+
+//             if (!masonry) {
+//                 masonry = new Masonry('.gallery', {
+//                     itemSelector: '.imgContainer',
+//                     columnWidth: '.imgContainer',
+//                     gutter: 15
+//                 });
+//             } else {
+//                 setTimeout(() => {
+//                     masonry.reloadItems(); // Recargar elementos para librería Masonry
+//                     masonry.layout(); // Volver a organizar con Masonry
+//                 }, 100);
+//             }
+//             } catch (error) {
+//                 console.log(error);
+//             }
+//         })
+//     }
+
+export const searchWord = async () => {
     const inputHTML = document.querySelector('#buscar');
-    const section = document.querySelector('.gallery')
+    const section = document.querySelector('.gallery');
+    let masonry;
+
     inputHTML.addEventListener('keyup', async (e) => {
         try {
             const word = e.target.value;
-                if(word === ''){
-                    showGallery();
-                }
-
-            const newImages = await fetch (`https://api.unsplash.com/search/photos?query=${word}&client_id=Apv4rswnoZ0Lp7xGFFJbnS520XpHA1J1dCSILs53y64&count=40`);
-            const element = await newImages.json();
-            section.innerHTML = '';
-            element.results.forEach(c => {
-                const img = document.createElement('img');
-                img.classList.add('photo');
-                img.src = c.urls.small;
-                section.appendChild(img);
-                
-            })
-            } catch (error) {
-                console.log(error);
+            if (word === '') {
+                showGallery();
+                return;
             }
-        })
-    }
+
+            let allResults = [];
+
+            // Hacer múltiples solicitudes para obtener más de 10 resultados
+            for (let page = 1; page <= 4; page++) { // Por ejemplo, obtener 40 resultados (4 páginas x 10 resultados)
+                const newImages = await fetch (`https://api.unsplash.com/search/photos?query=${word}&client_id=Apv4rswnoZ0Lp7xGFFJbnS520XpHA1J1dCSILs53y64&per_page=10&page=${page}`);
+                const element = await newImages.json();
+                allResults = allResults.concat(element.results);
+            }
+
+            section.innerHTML = '';
+
+            allResults.forEach(c => {
+                const img = document.createElement('img');
+                const imgContainer = document.createElement('div');
+
+                img.classList.add('photo');
+                imgContainer.classList.add('imgContainer');
+
+                img.src = c.urls.small;
+                imgContainer.appendChild(img);
+                section.appendChild(imgContainer);
+            });
+
+            if (!masonry) {
+                masonry = new Masonry('.gallery', {
+                    itemSelector: '.imgContainer',
+                    columnWidth: '.imgContainer',
+                    gutter: 15
+                });
+            } else {
+                setTimeout(() => {
+                    masonry.reloadItems();
+                    masonry.layout();
+                }, 200);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    });
+};
