abac379071c4742791d82c2e3c5b9a9ddb8a3efb
Carolina Buni <carolinabuni@gmail.com>
2024-01-17 13:22:10 +0100

    Arreglo rutas de las imágenes

--- 03-Async-Web-Design/src/components/Header/Header.js
+++ 03-Async-Web-Design/src/components/Header/Header.js
@@ -5,17 +5,17 @@ import './Header.css';
 // Agregar contenido al Header
 header.innerHTML += `
     <div class="header-links">
-        <img class="logo" src="/public/LogoPinterest.svg" alt="Logo Pinterest">
+        <img class="logo" src="/LogoPinterest.svg" alt="Logo Pinterest">
         <p>Inicio</p>
         <p>Explorar</p>
         <p>Crear</p>
     </div>
     <input type="search" name="search" id="buscar" placeholder="Buscar">
     <div class="header-icons">
-        <img src="/public/alert-icon.svg" alt="Alerts">
-        <img src="/public/messages-icon.svg" alt="Messages">
+        <img src="/alert-icon.svg" alt="Alerts">
+        <img src="/messages-icon.svg" alt="Messages">
         <img class="profile" src="https://i.pinimg.com/75x75_RS/50/01/2b/50012b40b879e0d913eb7f36af8d1fb8.jpg" alt="Profile">
-        <img src="/public/arrow-icon.svg" alt="Arrow">
+        <img src="arrow-icon.svg" alt="Arrow">
     </div>
     
 `
